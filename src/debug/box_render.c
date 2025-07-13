af5bd1b6072e2c7e0f3bd6a89a6294d641678d7c
DcraftBg <czl_obshta@abv.bg>
2025-07-13 16:18:03 +0300

    [render_box_html_tag] Implement render_box_html_tag

--- src/debug/box_render.c
+++ src/debug/box_render.c
@@ -1,43 +1,42 @@
 #include <debug/box_render.h>
+#include <bsrenderer/color.h>
+#include <bsrenderer/renderer.h>
+#include <bsrenderer/fill.h>
 #include <html.h>
 #include <stdio.h>
 
 size_t render_box_color_n = 0;
 void render_box_html_tag(BSRenderer* renderer, HTMLTag* tag, float scroll_y) {
-#if 0
-    static Color colors[] = {
-        GRAY,     
-        GOLD,     
-        PINK,     
-        SKYBLUE,  
-        RED,      
-        MAROON,   
-        GREEN,    
-        ORANGE,   
-        LIME,     
-        DARKGREEN,
-        BLUE,     
-        DARKBLUE, 
-        PURPLE,   
-        VIOLET,   
-        DARKPURPLE,
-        BEIGE,    
-        BROWN,    
-        DARKBROWN,
-        WHITE,    
-        BLANK,    
-        MAGENTA,  
-        RAYWHITE, 
+    static BSColor colors[] = {
+        0x828282ff, // GRAY
+        0xffcb00ff, // GOLD
+        0xff6dc2ff, // PINK
+        0x66bfffff, // SKYBLUE
+        0xe62937ff, // RED
+        0xbe2137ff, // MAROON
+        0x00e430ff, // GREEN
+        0xffa100ff, // ORANGE
+        0x009e2fff, // LIME
+        0x00752cff, // DARKGREEN
+        0x0079f1ff, // BLUE
+        0x0052acff, // DARKBLUE
+        0xc87affff, // PURPLE
+        0x873cbeff, // VIOLET
+        0x701f7eff, // DARKPURPLE
+        0xd3b083ff, // BEIGE
+        0x7f6a4fff, // BROWN
+        0x4c3f2fff, // DARKBROWN
+        0xffffffff, // WHITE
+        0x00000000, // BLANK
+        0xff00ffff, // MAGENTA
+        0xf5f5f5ff, // RAYWHITE
     };
-    DrawRectangle(tag->x, ((float)tag->y) + scroll_y, tag->width, tag->height, colors[render_box_color_n++]);
+    BSFill fill = {
+        .kind = BSFILL_SOLID_COLOR,
+        .as.color = colors[render_box_color_n++]
+    };
+    bsrenderer_draw_rectangle(renderer, tag->x, tag->y + scroll_y, tag->width, tag->height, &fill);
     for(size_t i = 0; i < tag->children.len; ++i) {
-        render_box_html_tag(tag->children.items[i], scroll_y);
+        render_box_html_tag(renderer, tag->children.items[i], scroll_y);
     }
-#else
-    // TODO: port render_box_html_tag to the generic renderer. I'm too lazy to unhardcode the raylib colors
-    fprintf(stderr, "<TBD port render_box_html_tag>\n");
-    (void)scroll_y;
-    (void)tag;
-    (void)renderer;
-#endif
 }
